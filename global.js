6711c6393ecc05ecb75efea2ef5bc1a63be56ca6
cubiotimer <33587165+cubiotimer@users.noreply.github.com>
2017-11-12 20:14:55 -0600

    Update global.js

--- global.js
+++ global.js
@@ -1,8 +1,13 @@
 var key;
+var newUser = localStorage.getItem('newUserLS') || true;
 
 window.onload = function () {
 	optionSel(0);
 	showScramType();
 	curSes = +localStorage.getItem('curSesLS') || 0;
 	loadSession(curSes);
+	if (newUser === true) {
+		alert('NOTICE:\n\nThis is still very early in development!\nThere is many features that are not added, but are planned to be!\n\nCubio is not optimized or tested on any other browser execpt chrome (for now).');
+		localStorage.setItem('newUserLS', false);
+	}
 };
